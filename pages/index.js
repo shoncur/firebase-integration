e5a5a58cee86e96cbd297f16cadee520a42fdb09
Shaun Currie <77080371+shoncur@users.noreply.github.com>
2021-11-09 10:11:49 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Brand from "../components/brand"
+import NavBar from "../components/navbar"
+
+
+function Home() {
+  return (
+    <>
+      <NavBar/>
+       <Brand tagline="login options" title="home page"/>
+    </>
+  )
+}
+
+export default Home
